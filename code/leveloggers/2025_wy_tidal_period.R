1530ee4e1ac1d9b8b9eacf41f5dc30265afb6f2a
fjoyce <fhjoyce@ucsb.edu>
2026-03-17 20:13:46 -0700

    fingers crossed for no conflict...

--- code/leveloggers/2025_wy_tidal_period.R
+++ code/leveloggers/2025_wy_tidal_period.R
@@ -16,11 +16,11 @@
 #also uses objects from 2025_wy_tides.R
 source("code/tides/2025_wy_tides.R")
 
-
 #filter levelogger data to relevant date range
 tidal_2025_wy_wse <- wse_2025_combined %>% 
   filter(date >= as.Date("2025-01-31")) %>% 
-  filter(date < as.Date("2025-03-08"))
+  filter(date <= as.Date("2025-04-11"))
+
 
 #and also uses objects from 2025_wy_precip.R
 source("code/met_station/2025_wy_precip.R")
@@ -28,22 +28,25 @@ source("code/met_station/2025_wy_precip.R")
 #filter rainfall to relevant range of dates
 tidal_2025_wy_rain <- water_year_2025 %>% 
   filter(date >= as.Date("2025-01-31")) %>% 
-  filter(date < as.Date("2025-03-08"))
-  
+  filter(date <= as.Date("2025-04-11"))
 
+# plot water levels with tides ----  
 tidal_2025_wy_fig <- ggplot(data = tidal_2025_wy_wse, aes(x = datetime, y = wse)) +
-  #add lines for levelogger data
-  geom_line(aes(color = station)) +
   # add line for tidal prediction
-  geom_line(data = tides_2025_02, aes(x = datetime, y = pred)) +
+  geom_line(data = tides_2025, aes(x = datetime, y = pred), color = "darkgray") +
   ylab("Water surface elevation (ft)") +
   xlab("Date") +
-  scale_x_datetime(date_breaks = "5 days",
+  scale_x_datetime(date_breaks = "10 days",
                    date_labels = "%b %d",
-                   expand = c(0,NA)
-                   )
+                   expand = c(0,0)
+  ) +
+  #add lines for levelogger data
+  geom_line(aes(color = station))
+ 
 
 tidal_2025_wy_fig  
+
+ggplotly(tidal_2025_wy_fig)
 
 #note- there is more of a lag (offset) 
 #between the tidal predictions and Pier water elevations than I would have expected
@@ -55,15 +58,14 @@ tidal_2025_wy_rain_fig <- ggplot(data = tidal_2025_wy_rain, aes(x = date, y = pr
   geom_col() +
   xlab("Date") +
   ylab("Rainfall (in)") +
-  scale_x_date(date_breaks = "5 days",
+  scale_x_date(date_breaks = "10 days",
                    date_labels = "%b %d",
-                   expand = c(0,NA)
+                   expand = c(0,0)
   )
                                   
 tidal_2025_wy_rain_fig
 
 #assemble water levels, tides, rainfall ----
-
 tidal_2025_fig <- plot_grid(
   tidal_2025_wy_fig,
   tidal_2025_wy_rain_fig,
@@ -73,6 +75,8 @@ tidal_2025_fig <- plot_grid(
 
 
 tidal_2025_fig
+
+
 
 #FIXME- need to get x axes to align correctly
 
